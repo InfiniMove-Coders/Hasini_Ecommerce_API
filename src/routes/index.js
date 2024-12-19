49b80b009c7df7fb337534ec62e917881d67ba3a
venkataPhanindraVutla <phanivutla2004@gmail.com>
2024-12-19 18:48:31 +0530

    added crud for address

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ const userRoutes = require("./userRoutes");
 const productRoutes = require("./productRoutes");
 const categoryRoutes = require("./categoryRoutes");
 const orderRoutes = require("./orderRoutes");
+const addressRoutes = require("./addressRoutes");
 
 router.use("/user", userRoutes);
 router.use("/products", productRoutes);
 router.use("/category", categoryRoutes);
 router.use("/order", orderRoutes);
+router.use("/address", addressRoutes);
 
 module.exports = router;
