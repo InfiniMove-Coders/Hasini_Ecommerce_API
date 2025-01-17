0f77fecc7c65d3fc3d3021fe82a28f6dd87ec81a
saijayanth59 <sithlord5995@gmail.com>
2025-01-17 18:58:55 +0530

    feat: implement pagination(page, pageSize) for orders for GET / along with filters

--- src/repositories/orderRepository.js
+++ src/repositories/orderRepository.js
@@ -14,12 +14,24 @@ class OrderRepository extends CrudRepository {
     }
   };
 
-  findAll = async (filter = {}) => {
+  findAll = async (filters = {}) => {
     try {
-      return await this.model
-        .find(filter)
+      let { page, pageSize, ...queryFilters } = filters;
+      let query = this.model
+        .find(queryFilters)
         .populate("products.product user shippingAddress")
         .sort({ createdAt: -1 });
+      if (page && pageSize) {
+        page = parseInt(page);
+        pageSize = parseInt(pageSize);
+        const skip = (page - 1) * pageSize;
+        console.log(skip, pageSize);
+        query = query.skip(skip).limit(pageSize);
+      }
+
+      const orders = await query.exec();
+      console.log(orders);
+      return orders;
     } catch (error) {
       throw new Error("Failed to retrieve orders");
     }
